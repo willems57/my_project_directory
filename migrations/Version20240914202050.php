747bb4865b7f06158f942f4e79cf2173a769db41
willems57 <jeanwillems@hotmail.fr>
2024-09-14 22:21:13 +0200

    execution de l migration

---
+++ migrations/Version20240914202050.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240914202050 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE animals (id INT AUTO_INCREMENT NOT NULL, rapportsverinaires_id INT DEFAULT NULL, picture_id INT DEFAULT NULL, habitat_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, etat VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_966C69DDC16753AD (rapportsverinaires_id), INDEX IDX_966C69DDEE45BDBF (picture_id), INDEX IDX_966C69DDAFFE2D26 (habitat_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE avis (id INT AUTO_INCREMENT NOT NULL, speudo VARCHAR(255) NOT NULL, comentaire LONGTEXT NOT NULL, visible TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE habitat (id INT AUTO_INCREMENT NOT NULL, picture_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, commentaires LONGTEXT NOT NULL, INDEX IDX_3B37B2E8EE45BDBF (picture_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, createdat DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updatedat DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE race (id INT AUTO_INCREMENT NOT NULL, animals_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, INDEX IDX_DA6FBBAF132B9E58 (animals_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE rapportsverinaires (id INT AUTO_INCREMENT NOT NULL, date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', details LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, service_id INT DEFAULT NULL, description VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_57698A6AED5CA9E6 (service_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, nameservice VARCHAR(50) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, role_id INT DEFAULT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, INDEX IDX_1D1C63B3D60322AC (role_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE zoo (id INT AUTO_INCREMENT NOT NULL, service_id INT NOT NULL, name VARCHAR(32) NOT NULL, description LONGTEXT NOT NULL, amopeningtime LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', pmopeningtime LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', enclosures INT NOT NULL, createdat DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updatedat DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_994F3F35ED5CA9E6 (service_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE animals ADD CONSTRAINT FK_966C69DDC16753AD FOREIGN KEY (rapportsverinaires_id) REFERENCES rapportsverinaires (id)');
+        $this->addSql('ALTER TABLE animals ADD CONSTRAINT FK_966C69DDEE45BDBF FOREIGN KEY (picture_id) REFERENCES picture (id)');
+        $this->addSql('ALTER TABLE animals ADD CONSTRAINT FK_966C69DDAFFE2D26 FOREIGN KEY (habitat_id) REFERENCES habitat (id)');
+        $this->addSql('ALTER TABLE habitat ADD CONSTRAINT FK_3B37B2E8EE45BDBF FOREIGN KEY (picture_id) REFERENCES picture (id)');
+        $this->addSql('ALTER TABLE race ADD CONSTRAINT FK_DA6FBBAF132B9E58 FOREIGN KEY (animals_id) REFERENCES animals (id)');
+        $this->addSql('ALTER TABLE role ADD CONSTRAINT FK_57698A6AED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id)');
+        $this->addSql('ALTER TABLE utilisateur ADD CONSTRAINT FK_1D1C63B3D60322AC FOREIGN KEY (role_id) REFERENCES role (id)');
+        $this->addSql('ALTER TABLE zoo ADD CONSTRAINT FK_994F3F35ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE animals DROP FOREIGN KEY FK_966C69DDC16753AD');
+        $this->addSql('ALTER TABLE animals DROP FOREIGN KEY FK_966C69DDEE45BDBF');
+        $this->addSql('ALTER TABLE animals DROP FOREIGN KEY FK_966C69DDAFFE2D26');
+        $this->addSql('ALTER TABLE habitat DROP FOREIGN KEY FK_3B37B2E8EE45BDBF');
+        $this->addSql('ALTER TABLE race DROP FOREIGN KEY FK_DA6FBBAF132B9E58');
+        $this->addSql('ALTER TABLE role DROP FOREIGN KEY FK_57698A6AED5CA9E6');
+        $this->addSql('ALTER TABLE utilisateur DROP FOREIGN KEY FK_1D1C63B3D60322AC');
+        $this->addSql('ALTER TABLE zoo DROP FOREIGN KEY FK_994F3F35ED5CA9E6');
+        $this->addSql('DROP TABLE animals');
+        $this->addSql('DROP TABLE avis');
+        $this->addSql('DROP TABLE habitat');
+        $this->addSql('DROP TABLE picture');
+        $this->addSql('DROP TABLE race');
+        $this->addSql('DROP TABLE rapportsverinaires');
+        $this->addSql('DROP TABLE role');
+        $this->addSql('DROP TABLE service');
+        $this->addSql('DROP TABLE utilisateur');
+        $this->addSql('DROP TABLE zoo');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
