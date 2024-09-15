e7e1f7757ad1621749053bd04eebbc1113b52d29
willems57 <jeanwillems@hotmail.fr>
2024-09-15 20:57:10 +0200

    controller d'essaye finaliser

--- src/Controller/ZooController.php
+++ src/Controller/ZooController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Zoo;
+use App\Repository\ZooRepository;
+use Doctrine\DBAL\Types\DateTimeImmutableType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,32 +13,60 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('api/zoo', name: 'app_api_zoo')]
 class ZooController extends AbstractController
 {
+public function __construct(private EntityManagerInterface $manager, private ZooRepository $zooRepository)
+{
+
+}
+
     #[Route(name: 'new', methods: 'post')]
     public function new(): response
     {
         $zoo = new Zoo();
         $zoo->setName('Arcadia');
         $zoo->setDescription('ce zoo fait le bonheur de tous ces visiteur depuis 1963.');
-       $zoo->setCreatedAt(new DateTimeImmutable(1963));
+       $zoo->setCreatedAt(new \DateTimeImmutable());
+
+       $this->manager->persist($zoo);
+       $this->manager->flush();
 //a stoquer en bas de donnee
 return $this->json(['message' => "zoo resource created with {$zoo->getId()} id"],
     
                 Response::HTTP_CREATED,);
 
     }
-    #[Route('/show', name: 'show', methods: 'get')]
-    public function show(): response
+    #[Route('/{id}', name: 'show', methods: 'get')]
+    public function show(int $id): Response
     {
-        return $this->json(['message'=> 'zoo de ma BDD']);
+        $zoo = $this->zooRepository->findOneBy(['id' => $id]);
+        if (!$zoo) 
+        {throw new \Exception('No zoo found for {$id} id');}
+        
+        return $this->json(['message' => "A zoo was found : {$zoo->getName()} for {$zoo->getId()} id"]);   
     }
-    #[Route('/', name:'edit', methods: 'put')]
-    public function edit(): response
-    {
 
+    #[Route('/{id}', name:'edit', methods: 'put')]
+    public function edit(int $id): response
+    {
+        $zoo = $this->zooRepository->findOneBy(['id' => $id]);
+          if (!$zoo) 
+        {throw new \Exception("No zoo found for {$id} id");}
+ 
+        $zoo->setName('zoo name updated');
+
+        $this->manager->flush();
+
+        return $this->redirectToRoute('app_api_zoo_show', ['id' => $zoo->getId()]);
     }
-    #[Route('/', name: 'delete', methods: 'delete')]
-    public function delete(): response
-    {
 
+    #[Route('/{id}', name: 'delete', methods: 'delete')]
+    public function delete(int $id): response
+    {
+        $zoo = $this->zooRepository->findOneBy(['id' => $id]);
+        if (!$zoo) 
+        {throw new \Exception("No zoo found for {$id} id");}
+
+        $this->manager->remove($zoo);
+        $this->manager->flush();
+        return $this->json(['message'=> 'zoo ressource delated'], Response::HTTP_NO_CONTENT);
     }
 }
