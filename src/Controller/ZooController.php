277c3dd201983d239e1e6497e5280e43b3218188
willems57 <jeanwillems@hotmail.fr>
2024-09-15 18:45:35 +0200

    creation d'un objets

--- src/Controller/ZooController.php
+++ src/Controller/ZooController.php
@@ -12,9 +12,17 @@ class ZooController extends AbstractController
     #[Route(name: 'new', methods: 'post')]
     public function new(): response
     {
+        $zoo = new Zoo();
+        $zoo->setName('Arcadia');
+        $zoo->setDescription('ce zoo fait le bonheur de tous ces visiteur depuis 1963.');
+       $zoo->setCreatedAt(new DateTimeImmutable(1963));
+//a stoquer en bas de donnee
+return $this->json(['message' => "zoo resource created with {$zoo->getId()} id"],
+    
+                Response::HTTP_CREATED,);
 
     }
-    #[Route('/', name: 'show', methods: 'get')]
+    #[Route('/show', name: 'show', methods: 'get')]
     public function show(): response
     {
         return $this->json(['message'=> 'zoo de ma BDD']);
