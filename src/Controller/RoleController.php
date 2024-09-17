afb8cc2a88c219afb9be50c89993796faf4a77cf
willems57 <jeanwillems@hotmail.fr>
2024-09-17 19:37:49 +0200

    revision de UtilisateurController RoleController

--- src/Controller/RoleController.php
+++ src/Controller/RoleController.php
@@ -2,17 +2,94 @@
 
 namespace App\Controller;
 
+use App\Entity\Role;
+use App\Repository\RoleRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class RoleController extends AbstractController
 {
-    #[Route('/role', name: 'app_role')]
-    public function index(): Response
+    public function __construct(
+    private EntityManagerInterface $manager,
+    private RoleRepository $RoleRepository,
+    private SerializerInterface $serializer,
+    private UrlGeneratorInterface $urlhenerator
+    )
     {
-        return $this->render('role/index.html.twig', [
-            'controller_name' => 'RoleController',
-        ]);
+    
     }
-}
+    
+        #[Route(name: 'new', methods: 'post')]
+        public function new(Request $request): JsonResponse
+        {
+            $Role = $this->serializer->deserialize($request->getContent(), Role::class, 'json');
+           $Role->setCreatedAt(new \DateTimeImmutable());
+    
+           $this->manager->persist($Role);
+           $this->manager->flush();
+    //a stoquer en bas de donnee
+    $responseData = $this->serializer->serialize($Role, 'json');
+    
+            $location = $this->urlhenerator->generate(
+                 'app_api_Role_show',
+                 ['id' => $Role->getId()],
+                 UrlGeneratorInterface::ABSOLUTE_URL,
+            );
+            return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
+    
+        }
+        #[Route('/{id}', name: 'show', methods: 'get')]
+        public function show(int $id): JsonResponse
+        {
+            $Role = $this->RoleRepository->findOneBy(['id' => $id]);
+            if (!$Role) 
+            {$responsedata=$this->serializer->serialize($Role, format: 'Json');
+            
+                return new JsonResponse($responsedata, Response::HTTP_OK, [], true);
+            }
+            
+            return new JsonResponse(null, Response::HTTP_NOT_FOUND);   
+        }
+    
+        #[Route('/{id}', name:'edit', methods: 'put')]
+        public function edit(int $id, Request $Request): JsonResponse
+        {
+        $Role=$this->RoleRepository->findOneBy(['id' => '$id']);
+        if ($Role){
+            $Role = $this->serializer->deserialize(
+            $Request->getContent(),
+            type: $Role::class,
+            format: 'Json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $Role] 
+            );
+        $Role->setUpdatedAt(new DateTimeImmutable());
+        $this->manager->flush();
+    
+        return new JsonResponse( null, Response::HTTP_NO_CONTENT);
+        }
+    
+        return new JsonResponse(data: null, status: Response::HTTP_NOT_FOUND);
+        }
+    
+        #[Route('/{id}', name: 'delete', methods: 'delete')]
+        public function delete(int $id): JsonResponse
+        {
+            $Role = $this->RoleRepository->findOneBy(['id' => $id]);
+            if (!$Role) {
+            $this->manager->remove($Role);
+            $this->manager->flush();
+    
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+    
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        }
+    }
--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,17 +2,94 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use App\Repository\UtilisateurRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class UtilisateurController extends AbstractController
 {
-    #[Route('/utilisateur', name: 'app_utilisateur')]
-    public function index(): Response
+    public function __construct(
+    private EntityManagerInterface $manager,
+    private UtilisateurRepository $utilisateurRepository,
+    private SerializerInterface $serializer,
+    private UrlGeneratorInterface $urlhenerator
+    )
     {
-        return $this->render('utilisateur/index.html.twig', [
-            'controller_name' => 'UtilisateurController',
-        ]);
+    
     }
-}
+    
+        #[Route(name: 'new', methods: 'post')]
+        public function new(Request $request): JsonResponse
+        {
+            $Utilisateur = $this->serializer->deserialize($request->getContent(), Utilisateur::class, 'json');
+           $Utilisateur->setCreatedAt(new \DateTimeImmutable());
+    
+           $this->manager->persist($Utilisateur);
+           $this->manager->flush();
+    //a stoquer en bas de donnee
+    $responseData = $this->serializer->serialize($Utilisateur, 'json');
+    
+            $location = $this->urlhenerator->generate(
+                 'app_api_Utilisateur_show',
+                 ['id' => $Utilisateur->getId()],
+                 UrlGeneratorInterface::ABSOLUTE_URL,
+            );
+            return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
+    
+        }
+        #[Route('/{id}', name: 'show', methods: 'get')]
+        public function show(int $id): JsonResponse
+        {
+            $Utilisateur = $this->utilisateurRepository->findOneBy(['id' => $id]);
+            if (!$Utilisateur) 
+            {$responsedata=$this->serializer->serialize($Utilisateur, format: 'Json');
+            
+                return new JsonResponse($responsedata, Response::HTTP_OK, [], true);
+            }
+            
+            return new JsonResponse(null, Response::HTTP_NOT_FOUND);   
+        }
+    
+        #[Route('/{id}', name:'edit', methods: 'put')]
+        public function edit(int $id, Request $Request): JsonResponse
+        {
+        $Utilisateur=$this->utilisateurRepository->findOneBy(['id' => '$id']);
+        if ($Utilisateur){
+            $Utilisateur = $this->serializer->deserialize(
+            $Request->getContent(),
+            type: $Utilisateur::class,
+            format: 'Json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $Utilisateur] 
+            );
+        $Utilisateur->setUpdatedAt(new DateTimeImmutable());
+        $this->manager->flush();
+    
+        return new JsonResponse( null, Response::HTTP_NO_CONTENT);
+        }
+    
+        return new JsonResponse(data: null, status: Response::HTTP_NOT_FOUND);
+        }
+    
+        #[Route('/{id}', name: 'delete', methods: 'delete')]
+        public function delete(int $id): JsonResponse
+        {
+            $Utilisateur = $this->utilisateurRepository->findOneBy(['id' => $id]);
+            if (!$Utilisateur) {
+            $this->manager->remove($Utilisateur);
+            $this->manager->flush();
+    
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+    
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        }
+    }
